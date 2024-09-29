05c7897ce5298850a9e4a632836048c654cba741
raphael <rafavfx1@gmail.com>
2024-09-29 20:54:03 -0300

    adicionando comentarios das propriedades do dto da aplicação

--- src/courses/dto/course.dto.ts
+++ src/courses/dto/course.dto.ts
@@ -1,5 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateCourseDTO {
+
+    @ApiProperty({type: 'number', description: 'ID' })
     public id: number;
+    
+    
+    @ApiProperty({type: 'string', description: 'Titulo do curso' })
     public title: string;
+    
+    
+    @ApiProperty({type: 'string', description: 'Descrição do curso' })
     public description: string;
 }
